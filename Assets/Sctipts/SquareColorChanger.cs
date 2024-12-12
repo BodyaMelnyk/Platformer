80678e61d959d0d716d24d79b602e6f6602a169a
BodyaMelnyk <bodyamelnyk2003@gmail.com>
2024-12-12 15:02:02 +0200

    color changing was added

---
+++ Assets/Sctipts/SquareColorChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareColorChanger : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _renderer.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+    }
+
+
+
+}
