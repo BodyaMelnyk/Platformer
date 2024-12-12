2a3e1f6a95b361857d90953842477c37a4c950d5
BodyaMelnyk <bodyamelnyk2003@gmail.com>
2024-12-12 15:45:42 +0200

    Square position was changed

--- Assets/Sctipts/SquareColorChanger.cs
+++ Assets/Sctipts/SquareColorChanger.cs
@@ -14,6 +14,7 @@ public class SquareColorChanger : MonoBehaviour
     private void Update()
     {
         _renderer.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+        transform.position = new Vector2(1f, 1f);
     }
 
 
